c93056a54c05824f2b57de863b2a8cd773842fc3
Ana-Ichimura <47996583+Ana-Ichimura@users.noreply.github.com>
2022-01-27 19:50:16 -0300

    formulario de insert

---
+++ cardapio-digital-web/src/app/modules/cardapio/components/cardapio-insert/cardapio-insert.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cardapio-insert',
+  templateUrl: './cardapio-insert.component.html',
+  styleUrls: ['./cardapio-insert.component.css']
+})
+export class CardapioInsertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
